f0fabe2e51c6da770612079ed686b63bcdd00afb
Karl-Ustav Kõlar <karlustavkolar@Karl-Ustavs-MacBook-Air.local>
2025-03-18 15:01:20 +0200

    oliveri main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Valjak valjak = new Valjak();
+        Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            System.out.println(Arrays.deepToString(valjak.getValjak())); // prindi väljak, tuleb panna pärast sinna kus on suunainput valitud
+            if (valjak.checkGameOver()) {
+                System.out.println("Mäng on läbi");
+                System.out.println("Lõppskoor: " + skoor);
+                System.exit(0); // lõpeta programmi töö
+            }
+            else {
+                System.out.println("");
+                String suund = scanner.nextLine();
+            }
+        }
     }
 }
