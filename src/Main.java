19282127417652adbc51bfe5675ce5a7cb272f58
StriKuR <oliverpiir25@gmail.com>
2025-03-18 14:06:16 +0200

    while true, gameover check

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Valjak valjak = new Valjak();
-
-
+        Scanner scanner = new Scanner(System.in);
 
         while(true) {
+            System.out.println(Arrays.deepToString(valjak.getValjak())); // prindi väljak
             if (valjak.checkGameOver()) {
-
+                System.out.println("Mäng on läbi");
+                System.out.println("Lõppskoor: " + skoor);
+                System.exit(0); // lõpeta programmi töö
+            }
+            else {
+                System.out.println("");
+                String suund = scanner.nextLine();
             }
         }
     }
